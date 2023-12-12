90d1a1829b79f05c153113ee56f7d7416e4d8eb8
ddnsramos <149492269+ddnsramos@users.noreply.github.com>
2023-12-12 18:01:33 -0500

    Update install_httpd.sh

--- prod/webservers/install_httpd.sh
+++ prod/webservers/install_httpd.sh
@@ -2,11 +2,12 @@
 yum -y update
 yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+myenv="Prod"
 echo "<h1><center>GROUP 4 ACS730 FINAL PROJECT<br>Members:<br> Daphne Denise Ramos <br>
 Jonalyn Ulloa <br>
 Rose Ann Camantes <br>
 Michael Concepcion <br>
 Augustine Opoku Junior Antwi <br>
-My IP is $myip."  >  /var/www/html/index.html
+My IP is $myip.<br> My environment is $myenv."  >  /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
